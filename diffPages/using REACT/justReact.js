a346e1a664759843ff9c18d055fef8123b59d147
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-03-08 14:46:54 +0530

    updated

---
+++ diffPages/using REACT/justReact.js
@@ -0,0 +1,9 @@
+const header = (
+  <div>
+    <h1 class="header"> Hello </h1>
+    <p>Welcome to the session</p>
+    <button class="btn">Click</button>
+  </div>
+);
+
+ReactDOM.creatRoot(document.getElementById("root")).render(header);
