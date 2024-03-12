3af23e97198953ca81371c9147cfcc44efa4b1b9
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-03-12 19:32:12 +0530

    updated
    updated

---
+++ helloword/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to React Course on codingninjas.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
