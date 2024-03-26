78a9e6c8af683319a74626f2a28f10284a6f0ad1
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-03-26 13:02:20 +0530

    updated

--- reactapp/src/Moviecard.js
+++ reactapp/src/Moviecard.js
@@ -11,21 +11,49 @@ class MovieCard extends React.Component {
         "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
       rating: "8.0",
       price: 199,
+      stars: 0,
     };
+    // this.addStars = this.addStars.bind(this);
   }
-  addStars(){
-    console.log("star added");
+  addStars = () =>{ //arrow function will automatically bind to this
+    if (this.state.stars >= 5) {
+      return;
+    }
+    //form1
+    // this.setState({
+    //   stars: this.state.stars +0.5
+    // });
+
+    //form2
+    this.setState((prevState) => {
+      return{
+        stars: prevState.stars +0.5
+      }
+    })
+    // this.state.stars += 0.5;
+    // console.log("this: ", this.state.stars);
+  }
+
+  removeStars = ()=>{
+    if(this.state.stars<=0){
+      return;
+    }
+     this.setState((prevState) => {
+       return {
+         stars: prevState.stars - 0.5
+       };
+     });
   }
 
   render() {
     //Destructing the state object in render function
-    const { title, plot, poster, price, rating } = this.state;
+    const { title, plot, poster, price, rating, stars } = this.state;
 
     return (
       <div className="main">
         <div className="movie-card">
           <div className="left">
-            <img alt="poster" src={poster}/>
+            <img alt="poster" src={poster} />
           </div>
           <div className="right">
             <div className="title">{title}</div>
@@ -38,6 +66,7 @@ class MovieCard extends React.Component {
                   className="str-btn"
                   alt="Decrease"
                   src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"
+                  onClick={this.removeStars}
                 />
                 <img
                   alt="star"
@@ -51,11 +80,11 @@ class MovieCard extends React.Component {
                   onClick={this.addStars}
                 />
 
-                <span className="starCount">0</span>
+                <span className="starCount">{stars}</span>
               </div>
 
               <button className="favourite-btn"> Favorite</button>
-              <button className="cart-btn"> Add t cart</button>
+              <button className="cart-btn"> Add to cart</button>
             </div>
           </div>
         </div>
