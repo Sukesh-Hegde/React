edc50df83b969c19ff721277781c688cd1e3c5ae
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-05-20 10:58:51 +0530

    updated

--- reactapp/src/Moviecard.js
+++ reactapp/src/Moviecard.js
@@ -1,49 +1,46 @@
-
 function MovieCard(props) {
+  const { movies, addStars, onDecStars, onClickFav, onClickAddtocart } = props;
 
-      const { movies, addStars, onDecStars, onClickFav, onClickAddtocart } =
-        props;
+  const { title, plot, poster, price, rating, stars, fav, isIncart } =
+    props.movies;
 
-    const { title, plot, poster, price, rating, stars, fav, isIncart } =
-      props.movies;
+  return (
+    <div className="main">
+      <div className="movie-card">
+        <div className="left">
+          <img alt="poster" src={poster} />
+        </div>
+        <div className="right">
+          <div className="title">{title}</div>
+          <div className="plot">{plot}</div>
+          <div className="price">Rs. {price}</div>
+          <div className="footer">
+            <div className="rating">{rating}</div>
+            <div className="star-dis">
+              <img
+                className="str-btn"
+                alt="Decrease"
+                src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"
+                onClick={() => onDecStars(movies)}
+              />
+              <img
+                alt="star"
+                src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"
+                className="stars"
+              />
+              <img
+                className="str-btn"
+                alt="increase"
+                src="https://cdn-icons-png.flaticon.com/128/2997/2997933.png"
+                onClick={() => {
+                  addStars(movies);
+                }}
+              />
 
-    return (
-      <div className="main">
-        <div className="movie-card">
-          <div className="left">
-            <img alt="poster" src={poster} />
-          </div>
-          <div className="right">
-            <div className="title">{title}</div>
-            <div className="plot">{plot}</div>
-            <div className="price">Rs. {price}</div>
-            <div className="footer">
-              <div className="rating">{rating}</div>
-              <div className="star-dis">
-                <img
-                  className="str-btn"
-                  alt="Decrease"
-                  src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"
-                  onClick={() => onDecStars(movies)}
-                />
-                <img
-                  alt="star"
-                  src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"
-                  className="stars"
-                />
-                <img
-                  className="str-btn"
-                  alt="increase"
-                  src="https://cdn-icons-png.flaticon.com/128/2997/2997933.png"
-                  onClick={() => {
-                    addStars(movies);
-                  }}
-                />
+              <span className="starCount">{stars}</span>
+            </div>
 
-                <span className="starCount">{stars}</span>
-              </div>
-
-              {/* {fav ? (
+            {/* {fav ? (
                 <button className="unfavourite-btn" onClick={handleFav}>
                   Un-Favorite{" "}
                 </button>
@@ -53,25 +50,26 @@ function MovieCard(props) {
                 </button>
               )} */}
 
-              <button
-                className={fav ? "unfavourite-btn" : "favourite-btn"}
-                onClick={() => onClickFav(movies)}
-              >
-                {fav ? "Un-Favorite" : "Favorite"}
-              </button>
+            {/* color changing of Fav button  */}
+            <button
+              className={fav ? "unfavourite-btn" : "favourite-btn"}
+              onClick={() => onClickFav(movies)}
+            >
+              {fav ? "Un-Favorite" : "Favorite"}
+            </button>
 
-              <button
-                className={isIncart ? "unfavourite-btn" : "cart-btn"}
-                onClick={() => onClickAddtocart(movies)}
-              >
-                {isIncart ? "Remove from Cart" : "Add to Cart"}
-              </button>
-            </div>
+            {/* color changing of Add to Cart button  */}
+            <button
+              className={isIncart ? "unfavourite-btn" : "cart-btn"}
+              onClick={() => onClickAddtocart(movies)}
+            >
+              {isIncart ? "Remove from Cart" : "Add to Cart"}
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
-
+    </div>
+  );
+}
 
 export default MovieCard;
