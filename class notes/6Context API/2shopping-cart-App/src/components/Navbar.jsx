92d1d84ce7c93a92020d3f6de47da6394a35b316
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-05-31 21:52:53 +0530

    updated

--- class notes/6Context API/2shopping-cart-App/src/components/Navbar.jsx
+++ class notes/6Context API/2shopping-cart-App/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "../styles/Total.module.css";
-import { itemContext } from "../itemContext";
-import { useContext } from "react";
+import { useValue } from "../itemContext";
+
 
 function Navbar() {
-  const {total, item} = useContext(itemContext)
+  const {total, item, handleReset} = useValue();
   return (
     <div className={styles.container}>
       <h1>Total : &#x20B9; {total}</h1>
       <h1>Items: {item}</h1>
+      <div className={styles.buttonsWrapper}>
+        <button className={styles.itemButton} onClick={() => handleReset()} >Reset</button>
+      </div>
+      
     </div>
   );
 }
--- class notes/6Context API/2shopping-cart-App/src/itemContext.js
+++ class notes/6Context API/2shopping-cart-App/src/itemContext.js
@@ -1,5 +1,43 @@
-import { createContext } from "react";
+import { useContext, useState, createContext } from "react";
 
 const itemContext = createContext();
 
-export {itemContext}
+//creatingcustom hook
+function useValue() {
+  const value = useContext(itemContext);
+  return value;
+}
+function CustomItemContext({ children }) {
+  const [total, setTotal] = useState(0);
+  const [item, setItem] = useState(0);
+
+  const handleAdd = (price) => {
+    setTotal(total + price);
+    setItem(item + 1);
+  };
+
+  const handleRemove = (price) => {
+    if (total <= 0) {
+      return;
+    }
+    // setTotal(total-price)
+    setTotal((prevState) => prevState - price);
+    setItem((prevState) => prevState - 1);
+  };
+  
+  const handleReset = () => {
+    setTotal(0);
+    setItem(0);
+  };
+
+  return (
+    <itemContext.Provider
+      value={{ item, total, handleAdd, handleRemove, handleReset }}
+    >
+      {children}
+    </itemContext.Provider>
+  );
+}
+
+export { itemContext, useValue };
+export default CustomItemContext;
