a61b15b7032409cc3f74ec1e9ae0dfb4fdb45f2a
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-06-02 09:50:35 +0530

    updated

--- class notes/7React Router/1Routing-creating-routes/src/App.js
+++ class notes/7React Router/1Routing-creating-routes/src/App.js
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Items from "./pages/Items";
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/about", element: <About /> },
+    { path: "/item", element: <Items /> },
   ]);
   return (
     <>
