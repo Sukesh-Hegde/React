4b846463c02f210025e0676ae25cf1990306c675
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-06-03 11:46:27 +0530

    updated

---
+++ class notes/7React Router/2E-Learning-App/src/App.js
@@ -0,0 +1,28 @@
+import Hero from "./pages/app/hero/Hero";
+import Nav from "./components/nav/Nav";
+import Courses from "./pages/app/courses/Courses";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const browserRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav />,
+      children: [
+        { index:true, element: <Hero /> },
+        {
+          path: "/courses",
+          element: <Courses />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={browserRouter} />
+    </>
+  );
+}
+
+export default App;
