5eaa5aa80b643b622ca80d2a0eeb8d0ebc04b4ea
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-02 15:15:50 +0530

    updated the redux in vanilla JS

--- class notes/8Redux/1Rudux in vanilla JS/todo-redux.js
+++ class notes/8Redux/1Rudux in vanilla JS/todo-redux.js
@@ -1,8 +1,59 @@
-const redux = require('redux')
+const redux = require("redux");
 
-//Actions
-const ADD_TODO = 'ADD_TODO';
+// action types
+const ADD_TODO = "ADD_TODO";
 const TOGGLE_TODO = "TOGGLE_TODO";
 
-//Action creators
-const addTodo = (text) => ({ })
+// action creators
+const addTodo = (text) => ({ text, type: ADD_TODO });
+const toggleTodo = (index) => ({ index, type: TOGGLE_TODO });
+
+// initial state
+const initialState = {
+  todos: [],
+};
+
+// reducer
+//must return updated state
+function todoReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false,
+          },
+        ],
+      };
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo, i) => {
+          if (i === action.index) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            };
+          }
+          return todo;
+        }),
+      };
+    default:
+      return state;
+  }
+}
+
+// store
+
+const store = redux.createStore(todoReducer);
+
+// dispatch actions
+store.dispatch(addTodo("Learn Redux"));
+store.dispatch(addTodo("Build an app"));
+store.dispatch(toggleTodo(0));
+
+// Read the Store
+console.log(store.getState());
