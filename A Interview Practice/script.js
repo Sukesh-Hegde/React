42e74ae08402af050bacfdbec6010a50683765c6
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-29 17:16:51 +0530

    updated

--- A Interview Practice/script.js
+++ A Interview Practice/script.js
@@ -169,3 +169,13 @@
 //   this.year = year;
 //   this.country = country;
 // }
+
+// **************************************************************************************
+
+// const today = new Date();
+
+// const day = today.getDate();
+// const month = today.getMonth();
+// const year = today.getFullYear();
+
+// console.log(`${day}-${month}-${year}`);
