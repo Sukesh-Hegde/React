66807ed17dc6e8a38514585cbbe35d052b84fd3b
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-09-01 19:56:39 +0530

    updated

--- A Interview Practice/script.js
+++ A Interview Practice/script.js
@@ -179,3 +179,26 @@
 // const year = today.getFullYear();
 
 // console.log(`${day}-${month}-${year}`);
+
+// **************************************************************************************
+// function removeDuplicate(arr) {
+//   let seen = {};
+
+//   let result = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let value = arr[i];
+
+//     if (!seen[value]) {
+//       result.push(value);
+//       seen[value] = true;
+//     }
+//   }
+//   return result;
+// }
+
+// input = [2, 1, -1, 2, 3, 3];
+// let output = removeDuplicate(input);
+
+// console.log(output);
+
