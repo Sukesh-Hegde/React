17fc5f83aec3ee2a2853b84fa7e99567ff51afb2
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-09-13 11:59:16 +0530

    updated

--- A Interview Practice/Timer/script.js
+++ A Interview Practice/Timer/script.js
@@ -25,3 +25,40 @@ function init() {
 }
 requestAnimationFrame(init);
 
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+
+// const getTimeString = () => {
+//   const hours = new Date().getHours();
+//   console.log(hours);
+//   if (hours >= 5 && hours < 12) {
+//     return "Good Morning";
+//   } else if (hours >= 12 && hours < 17) {
+//     return "good afternoon";
+//   } else {
+//     return "Good Night";
+//   }
+// };
+
+// export default function App() {
+//   const [time, setTime] = useState(new Date());
+
+//   useEffect(() => {
+//     const timer = setInterval(() => setTime(new Date()), 1000);
+//     return () => clearInterval(timer);
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>{getTimeString()}</h1>
+//       <h2>{time.toLocaleTimeString()}</h2>
+//     </div>
+//   );
+// }
